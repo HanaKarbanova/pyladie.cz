c6aa56430d689ff69243467949d8a191319589c6
Hana Karbanová <hana@karbanova.cz>
2016-11-20 18:19:18 +0100

    programy k desate lekci

---
+++ 10/zeme.py
@@ -0,0 +1,11 @@
+KODY_ZEMI={
+    'CZ': 'Ceska republika'
+    'Sk':'Slovenska republika'
+    'Dk':'Dansko'
+    'Ph':'Filipiny'
+    'Gl':'Gronsko'
+}
+
+while True:
+    vstup = input('zadej kod zeme:')
+    print(KODY_ZEMI[vstup])
